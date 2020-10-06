c7a15eab0fa0348fea8ab585cca988db4e4e786d
Igor <lemunruss1@gmail.com>
2020-10-06 20:32:31 +0300

    init: start Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+let obj  = Object.fromEntries(bracketsConfig);
+let arr = [];
+
+  for (let i of str.split('')) {
+      if (arr[0] === i) {
+         arr.shift();
+      } else if (obj[i]) {
+        arr.unshift(obj[i]);
+      } else {
+          return false;
+      }
+  }
+
+  return arr.length === 0;
 }
